ede1f4dada08961f28d779524fd4ca8dd75e1337
will-short <willofmetal85@gmail.com>
2021-12-07 16:59:29 -0600

    basic grid style done

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,6 +9,7 @@ import UsersList from "./components/UsersList";
 import User from "./components/User";
 import { authenticate } from "./store/session";
 import * as serverActions from "./store/servers";
+import Servers from "./components/Servers";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -19,7 +20,6 @@ function App() {
       await dispatch(authenticate());
       setLoaded(true);
     })();
-
   }, [dispatch]);
 
   if (!loaded) {
@@ -45,6 +45,9 @@ function App() {
         <ProtectedRoute path="/" exact={true}>
           <h1>My Home Page</h1>
         </ProtectedRoute>
+        <Route path="/servers">
+          <Servers />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
---
+++ react-app/src/components/Servers/index.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import style from "./servers.module.css";
+
+export default function Servers() {
+  return (
+    <main className={style.main}>
+      <div className={style.div1}></div>
+      <div className={style.div2}></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </main>
+  );
+}
