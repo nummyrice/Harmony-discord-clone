43f751ada5f9f711a66025425958e50c8b813413
nummyrice <nicholasroyrice@gmail.com>
2021-12-07 09:04:00 -0800

    delete mesage test

--- app/api/message_routes.py
+++ app/api/message_routes.py
@@ -48,44 +48,11 @@ def edit_message(serverId, channelId, messageId):
     return {'errors': validation_errors_to_error_messages(form.errors)}, 400
 
 
-# @server_routes.route('/<int:id>', methods=['DELETE'])
-# @login_required
-# def delete_server(id):
-#     server = Server.query.get(int(id))
-#     if server.owner_id == session.id:
-#         db.session.delete(server)
-#         db.session.commit()
-#         return {"result": "success"}
-
-
-# @server_routes.route('/<int:serverId>/members', methods=['POST'])
-# @login_required
-# def edit_members(serverId):
-#     userId = session.id
-#     user = User.query.get(int(userId))
-#     server = Server.query.get(int(serverId))
-#     if user and user not in server.members:
-#         member = Member(
-#             user_id=userId,
-#             server_id=serverId
-#         )
-#         db.session.add(member)
-#         db.session.commit()
-#         return server.to_dict()
-
-#     return {'errors': "bad user data"}
-
-
-# @server_routes.route('/<int:serverId>/members', methods=['DELETE'])
-# @login_required
-# def delete_member(serverId):
-#     userId = session.id
-#     user = User.query.get(int(userId))
-#     server = Server.query.get(int(serverId))
-#     if user in server.members:
-#         member = Member.query.filter(
-#             userId == Member.user_id and serverId == Member.server_id)[0]
-#         db.session.delete(member)
-#         db.session.commit()
-#         return {"result": "success"}
-#     return {"result": "failed"}
+@message_routes.route('/<int:serverId>/<int:channelId>/<int:messageId>', methods=['DELETE'])
+@login_required
+def delete_message(messageId):
+    message = Message.query.get(int(messageId))
+    if message.owner_id == session.id:
+        db.session.delete(message)
+        db.session.commit()
+        return {"result": "success"}
