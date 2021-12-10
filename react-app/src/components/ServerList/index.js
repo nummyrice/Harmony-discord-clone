41f1a5922ca701ea8b883dec8bcba49b6d405322
will-short <willofmetal85@gmail.com>
2021-12-09 18:32:32 -0600

    merged

--- react-app/src/components/ServerList/index.js
+++ react-app/src/components/ServerList/index.js
@@ -1,56 +1,43 @@
-import React, { useEffect } from 'react';
-import { NavLink } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-import * as serverActions from '../../store/servers'
+import * as serverActions from "../../store/servers";
 
-import style from './serversList.module.css';
-import logo from './images/white-logo.png';
+import style from "./serversList.module.css";
+import logo from "./images/white-logo.png";
 
-export default function ServersList({setServerActive}) {
-    const dispatch = useDispatch();
-    const servers = useSelector((state) => Object.values(state.servers));
+export default function ServersList({ setServerActive }) {
+  const dispatch = useDispatch();
+  const servers = useSelector((state) => Object.values(state.servers));
 
-    useEffect(() => {
-        dispatch(serverActions.getServersThunk())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(serverActions.getServersThunk());
+  }, [dispatch]);
 
-    return (
-        <div className={style.container}>
+  return (
+    <div className={style.container}>
+      <NavLink className={style.home} to={"/servers/@me"}>
+        <img className={style.directMessages} src={logo} />
+      </NavLink>
+      {servers?.map((server) => {
+        if (!server.private) {
+          return (
             <NavLink
-                className={style.home}
-                to={'/servers/@me'}
+              to={`/servers/${server.id}`}
+              className={style.serverWrapper}
+              key={server.id}
             >
-                <img
-                    className={style.directMessages}
-                    src={logo}
-                />
+              <img className={style.serverImg} src={server.image_url} />
             </NavLink>
-            { servers?.map(server => {
-                if (!server.private) {
-                    return(
-                        <NavLink
-                            to={`/servers/${server.id}`}
-                            className={style.serverWrapper}
-                            key={server.id}
-                        >
-                            <img
-                                className={style.serverImg}
-                                src={ server.image_url }
-                            />
-                        </NavLink>
-                    )
-                }
-            }
-            )}
-            <button
-                className={style.newServer}
-                onClick={() => setServerActive(true)}
-            >
-                <div className={style.newWrapper}>
-                    <div className={style.addServer}>+</div>
-                </div>
-            </button>
+          );
+        }
+      })}
+      <button className={style.newServer} onClick={() => setServerActive(true)}>
+        <div className={style.newWrapper}>
+          <div className={style.addServer}>+</div>
         </div>
-    )
+      </button>
+    </div>
+  );
 }
