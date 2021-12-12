6f9dcf77ab6d0d95457bccfd3ba17edcd67063cb
nummyrice <nicholasroyrice@gmail.com>
2021-12-12 11:08:21 -0800

    landing css almost complete

--- app/seeds/servers.py
+++ app/seeds/servers.py
@@ -4,7 +4,43 @@ from sqlalchemy.sql.expression import func
 
 def seed_servers():
     # adjust the range to add more or less seed data
-    for x in range(1, 5):
+    demoServer = Server(
+        name="App Academy Chat",
+        image_url="https://www.tinkeringmonkey.com/wp-content/uploads/2020/09/app-academy-closeup2-scaled.jpg",
+        private=False,
+        owner_id=1
+    )
+    db.session.add(demoServer)
+    db.session.commit()
+
+     # adds users 1 thru 5 to "App Academy Chat"
+    for y in range(1, 6):
+        demoMember = Member(
+            user_id=y,
+            server_id=1
+        )
+        db.session.add(demoMember)
+        db.session.commit()
+
+    demoPrivateServer = Server(
+        name="Boas Private Chat",
+        image_url="https://www.hospitalveterinariglories.com/wp-content/uploads/2019/11/19-11-27-boa-constrictor.jpg",
+        private=True,
+        owner_id=1
+    )
+    db.session.add(demoPrivateServer)
+    db.session.commit()
+
+    # adds users 1 thru 5 to "Boas Private Chat"
+    for z in range(1, 6):
+        demoMember = Member(
+            user_id=z,
+            server_id=2
+        )
+        db.session.add(demoMember)
+        db.session.commit()
+
+    for x in range(3, 16):
         random_user_id = db.session.query(User.id).order_by(
                 func.random()).first()[0]
         seed_server = Server(
@@ -32,12 +68,17 @@ def seed_servers():
             user_id=4,
             server_id=x
         )
+        make_member5 = Member(
+            user_id=5,
+            server_id=x
+        )
         db.session.add(make_member3)
         db.session.add(make_member)
         db.session.add(make_member2)
         db.session.add(make_member4)
-
+        db.session.add(make_member5)
         db.session.commit()
+
 
 def undo_servers():
     db.session.execute('TRUNCATE servers RESTART IDENTITY CASCADE')
