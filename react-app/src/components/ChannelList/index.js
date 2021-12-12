4152bdd58042265015fc07acd6776a0af0d9df0c
Rick Arocho <rick.arocho@gmail.com>
2021-12-12 01:05:09 -0600

    updated channel styling

--- react-app/src/components/ChannelList/index.js
+++ react-app/src/components/ChannelList/index.js
@@ -16,6 +16,8 @@ const ChannelList = () => {
   const server = useSelector((state) => state.servers?.[serverId]);
   let channels = useSelector((state) => state.servers?.[serverId]?.channels);
 
+  console.log('CHANNEL ID!!!-----: ', channelId);
+
   const [channelModalActive, setChannelModalActive] = useState(false);
   const [editChannelModalActive, setEditChannelModalActive] = useState(false);
 
@@ -47,7 +49,7 @@ const ChannelList = () => {
     <svg class="addButtonIcon-2CbG1X" aria-hidden="false" width="14" height="14" viewBox="0 0 18 18"><polygon points="15 10 10 10 10 15 8 15 8 10 3 10 3 8 8 8 8 3 10 3 10 8 15 8"></polygon></svg>
   );
 
-    const channelSettings = document.getElementById("editChannelModal");
+    // const channelSettings = document.getElementById("editChannelModal");
 
     let newChannelBtn;
     let editChannelBtn;
